72a9de288559590f4ef25e923190dd59c8b40b7c
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2015-04-12 14:46:50 +0200

    implement shouldComponentUpdate on BezierEditorAndPicker

--- client/ui/BezierEditorAndPicker/index.js
+++ client/ui/BezierEditorAndPicker/index.js
@@ -12,7 +12,31 @@ const selectedItemStyles = {
 
 };
 
+function sameValue (a, b) {
+  if (!a || !b) {
+    return a===b;
+  }
+  return a[0]===b[0] && a[1]===b[1] && a[2]===b[2] && a[3]===b[3];
+}
+
 export default class BezierEditorAndPicker extends React.Component {
+
+  shouldComponentUpdate (props) {
+    const {
+      value,
+      onChange,
+      width,
+      height,
+      progress
+    } = this.props;
+    return (
+      onChange !== props.onChange ||
+      width !== props.width ||
+      height !== props.height ||
+      progress !== props.progress ||
+      !sameValue(value, props.value)
+    );
+  }
 
   render () {
     const {
