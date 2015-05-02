7c48a99797aecd8ba3c1a9a5b473da1f45772cf7
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2015-05-02 17:43:06 +0200

    wider viewer

--- client/ui/App/index.js
+++ client/ui/App/index.js
@@ -151,7 +151,7 @@ var App = React.createClass({
       viewerW = Math.round(width / 2);
       viewerH = Math.round(viewerW * 3 / 4);
     }
-    viewerW = Math.max(width - 820, Math.min(280, viewerW));
+    viewerW = Math.max(width - 820, Math.min(400, viewerW));
     viewerH = Math.max(height - 300, viewerH);
 
     var viewerBound = {
