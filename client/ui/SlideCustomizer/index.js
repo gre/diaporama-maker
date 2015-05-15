48e844c837f8ba4dcd1cd34117fb9df3e5201cd4
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2015-05-15 22:03:35 +0200

    add more validation on SlideCustomizer

--- client/ui/SlideCustomizer/index.js
+++ client/ui/SlideCustomizer/index.js
@@ -1,4 +1,5 @@
 require('brace');
+import Slide2d from "slide2d";
 import React from 'react';
 import AceEditor from '../ReactAce';
 import beautify from "json-beautify";
@@ -11,26 +12,53 @@ export default class SlideCustomizer extends React.Component {
   constructor (props) {
     super(props);
     this.onChange = this.onChange.bind(this);
+    this.slide2d = Slide2d(document.createElement("canvas").getContext("2d"));
+    this.state = {
+      error: null
+    };
   }
 
   onChange (value) {
     try {
       var obj = JSON.parse(value);
+      this.slide2d.render(obj);
       this.props.onChange(obj);
+      this.setState({ error: null });
     }
-    catch (e) {}
+    catch (error) {
+      console.log(error);
+      this.setState({ error });
+    }
   }
 
   render () {
-    return <AceEditor
-      mode="json"
-      theme="solarized_dark"
-      onChange={this.onChange}
-      defaultValue={beautify(this.props.value, null, 2, 80)}
-      name="slide_customizer"
-      width={this.props.width + "px"}
-      height={this.props.height + "px"}
-    />;
+    const {
+      width,
+      height,
+      value
+    } = this.props;
+    const {
+      error
+    } = this.state;
+    const statusBarHeight = 20;
+    const statusBarStyle = {
+      width: width+"px",
+      height: statusBarHeight+"px",
+      background: "#002B36",
+      color: "#f33"
+    };
+    return <div>
+      <AceEditor
+        mode="json"
+        theme="solarized_dark"
+        onChange={this.onChange}
+        defaultValue={beautify(value, null, 2, 80)}
+        name="slide_customizer"
+        width={width + "px"}
+        height={(height-statusBarHeight) + "px"}
+      />
+    <div style={statusBarStyle}>{error && error.toString() || ""}</div>
+    </div>;
   }
 
 }
